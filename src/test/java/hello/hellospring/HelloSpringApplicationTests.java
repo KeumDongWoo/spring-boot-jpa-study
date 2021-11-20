49c6f7f7e4f207a4060d98282051deee33b5692d
KeumDongWoo <seletr96@gmail.com>
2021-11-20 19:08:00 +0900

    jpa , test code 작성

---
+++ src/test/java/hello/hellospring/HelloSpringApplicationTests.java
@@ -0,0 +1,18 @@
+package hello.hellospring;
+
+import org.apache.logging.log4j.message.Message;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@SpringBootTest
+class HelloSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
